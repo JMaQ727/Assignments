ba7c2ebbe8c56d4ed185955e6f7a383bc0ec2f14
justin-mcclain <justinamcclain@gmail.com>
2022-02-09 08:06:08 -0600

    forgot last loop

--- Python/For Loop Basic I/for_loops.py
+++ Python/For Loop Basic I/for_loops.py
@@ -12,12 +12,18 @@
 #     else:
 #         print(x)
 
-sum = 0
-for x in range (0,500001):
-    if x % 2 != 0:
-        sum = sum + x
-print(sum)
+# sum = 0
+# for x in range (0,500001):
+#     if x % 2 != 0:
+#         sum = sum + x
+# print(sum)
 
 # for x in range (2018, 0, -4):
 #     print(x)
 
+lownum = 2
+highnum = 9
+mult=3
+for x in range (lownum, highnum + 1):
+    if x % mult == 0:
+        print(x)
