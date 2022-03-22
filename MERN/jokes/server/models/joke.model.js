d661098f6228fc9d6e1265c281bbaa8d26d73f94
justin-mcclain <justinamcclain@gmail.com>
2022-03-22 18:16:34 -0500

    edit

--- MERN/jokes/server/models/joke.model.js
+++ MERN/jokes/server/models/joke.model.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const JokeSchema = new mongoose.Schema({
-    setup: {
-        type: String,
-        required: [true,"Setup must be at least 10 characters"],
-        minlength: [10, "Setup msut be at least 10 characters"],
+const JokeSchema = new mongoose.Schema(
+    {
+        setup: {
+            type: String,
+            required: [true, "Setup must be at least 10 characters"],
+            minlength: [10, "Setup msut be at least 10 characters"],
+        },
+        punchline: {
+            type: String,
+            required: [true, "Punchline must be at least 3 characters"],
+            minlength: [3, "Punchline must be at least 3 characters"],
+        },
     },
-    punchline: {
-        type: String,
-        required: [true, "Punchline must be at least 3 characters"],
-        minlength: [3, "Punchline must be at least 3 characters"],
-    },
-    },
-    {timestamps: true}
-)
+    { timestamps: true }
+);
 
-const Joke = mongoose.model('Joke', JokeSchema);
+const Joke = mongoose.model("Joke", JokeSchema);
 module.exports = Joke;
