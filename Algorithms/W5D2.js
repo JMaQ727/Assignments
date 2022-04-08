ab2c2c0d80f2d68c63f63800733294a5f5749076
justin-mcclain <justinamcclain@gmail.com>
2022-04-08 11:21:08 -0500

    edits

--- Algorithms/W5D2.js
+++ Algorithms/W5D2.js
@@ -88,25 +88,84 @@ class SinglyLinkedList {
             runner = runner.next
         }
     }
+    removeVal(val){
+        if (!this.contains(val)){
+            return false;
+        } else if(this.head.value == val) {
+            this.removeHead()
+            return true
+        } else{
+            var runner = this.head;
+            while (runner.next.value != val){
+                runner = runner.next;
+            }
+            runner.next = runner.next.next;
+            return true;
+        };
+    };
+    moveMintoFront() {
+        var runner = this.head
+        var min = 0
+        while (runner != null && runner.next != null) {
+            if (runner.value > runner.next.value) {
+                min = runner.next.value
+            }
+            runner = runner.next 
+        }
+        this.removeVal(min)
+        this.addToHead(min)
+    }
+    concat(list) {
+        var sll = []
+        var runner = list.head
+        while (runner != null) {
+            sll.push(runner.value)
+            runner = runner.next
+            console.log(sll)
+        }
+        for (var i = 0; i < sll.length; i++) {
+            this.addToTail(sll[i])
+        }
+    }
 }
 
 var new_sll = new SinglyLinkedList();
+var new_sll2 = new SinglyLinkedList();
 
 new_sll.addToHead(8);
-new_sll.addToHead(4);
+// new_sll.addToHead(4);
 new_sll.addToHead(6);
 new_sll.display(); // 6 - 3 - 8
 new_sll.addToTail(4);
 new_sll.display(); // 6 - 3 - 8 - 4
 new_sll.addToTail(7); // 6 - 3 - 8 - 4 - 7
+// new_sll.display();
+// new_sll.removeFront();
+// new_sll.display();
+// new_sll.removeBack();
 new_sll.display();
-new_sll.removeFront();
+new_sll.moveMintoFront();
 new_sll.display();
-new_sll.removeBack();
+new_sll2.addToHead(5);
+new_sll2.addToHead(2);
+new_sll2.addToHead(8);
+new_sll2.display();
+new_sll.concat(new_sll2);
 new_sll.display();
-new_sll.average();
 
 // console.log(new_sll.contains(4)); // true
 // console.log(new_sll.contains(6)); // true
 // console.log(new_sll.contains(7)); // true
 // console.log(new_sll.contains(11)); // false
+
+
+// concat(list) {
+//     var sll = []
+//     for (var i = 0; i < list.length; i++) {
+//         sll.push(list[i])
+//         console.log("ARRAY", sll)
+//     }
+//     for (var j = 0; j < sll.length - 1; j++) {
+//         new_sll.addToTail(ssl[j])
+//     }
+// }
